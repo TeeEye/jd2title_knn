d624fa0d90aa3297f8989a5a7167a6a5805fb07b
TeeEye <wangchenghao@mokahr.com>
2019-06-21 15:52:40 +0800

    initial commit

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
-DATA_PATH = '/data/t2t/jds_20.pkl'
-TFIDF_PATH = '/data/knn/jds_20_tfidf.pkl'
+DATA_PATH = '/data/t2t/jds_22.pkl'
+TFIDF_PATH = '/data/knn/jds_22_tfidf.pkl'
 SKILL_PATH = '/data/knn/skills.txt'
